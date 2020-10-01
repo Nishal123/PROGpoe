2d5aa45d29d88f075fe498d2ca192200665da1be
Nishal123 <60687949+Nishal123@users.noreply.github.com>
2020-10-01 13:30:26 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Class1
+    { 
+        progPOEEntities1 db = new progPOEEntities1();
+        String user = "Invalid";
+        
+        public String login(String Username, String Password)
+        {
+            foreach ( var s in db.Students)
+            {
+                if ((s.stu_Num == Convert.ToInt16(Username)) && (s.stu_Password == Password))
+                {
+                    user = "Student";
+                }
+                else 
+                {
+                    user = "Invalid";
+
+                }
+
+
+            }
+            foreach (Lecturer L in db.Lecturers)
+            {
+                if ((L.L_Firstname.Equals(Username)) && (L.L_Password == Password))
+                {
+                    user = "Lecturer";
+                }
+
+            }
+            return user;
+
+
+        }
+
+
+    }
+}
